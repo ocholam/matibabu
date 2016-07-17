3e9f46545a8ad3a0788de806a212fd8d5ac4d83c
ianmin2 <ianmin2@live.com>
2016-07-17 16:04:58 +0300

    :construction: Modified database schema

--- schema/hospital_care.sql
+++ schema/hospital_care.sql
@@ -33,6 +33,60 @@ INSERT INTO users
 VALUES 
 ('ianmin2',md5('ianmin2'),'Ian Innocent','ianmin2@live.com','0725678447');
 
+--- TYPES
+DROP TABLE IF EXISTS types CASCADE;
+CREATE TABLE IF NOT EXISTS types (
+    type_id      bigserial PRIMARY KEY,
+    title        varchar(100) NOT NULL CONSTRAINT unique_type UNIQUE
+);
+INSERT INTO types (title) VALUES ('Hospital'),('Doctor'),('Dentist');
+
+--- SUB_TYPES
+DROP TABLE IF EXISTS sub_types CASCADE;
+CREATE TABLE IF NOT EXISTS sub_types (
+    sub_type_id      bigserial PRIMARY KEY,
+    title            varchar(100) NOT NULL CONSTRAINT unique_sub_type UNIQUE,
+    parent           bigint NOT NULL CONSTRAINT valid_parent_type REFERENCES types(type_id)
+);
+INSERT INTO sub_types(title,parent) VALUES ('Hospital',1),('General Practitioner',2),('Peridontist',3);
+
+--- ENITITIES
+DROP TABLE IF EXISTS entities CASCADE;
+CREATE TABLE IF NOT EXISTS entities (
+    entity_id   bigserial PRIMARY KEY,
+    title       varchar(100) NOT NULL,
+    type        bigint NOT NULL CONSTRAINT valid_type REFERENCES sub_types(sub_type_id),
+    location    varchar(255),
+    telephone   varchar(20) CONSTRAINT unique_telephone UNIQUE,
+    office      varchar(20),
+    landline    varchar(20),
+    address     text,
+    fax         varchar(100),
+    email       varchar(50) CONSTRAINT unique_email UNIQUE,
+    web         varchar(100),
+    country     smallint DEFAULT '112',
+    others      text
+);
+INSERT INTO entities 
+ (title,type,location,telephone,office,landline,address,fax,email,web,others)
+ VALUES
+ ('Ian Innocent',3,'Chebarbar','0725678447','072345678','','Text Lane Off Limuru Road','','ianmin2@ianmin2.cf','http://ianmin2.cf',''),
+ ('Jeremic Hospital',1,'Baraton, Kapsabet','0700100100','','','1 Jeremic Close University of Eastern Africa Baraton','','jeremic@ueab.ac.ke','http://jeremic.ueab.ac.ke',''),
+ ('Chemundu Hospital',1,'Chemundu, Kapsabet','0700100101','','','1 Chemundu Drive; Chemundu','','chemundu@ueab.ac.ke','http://ueab.ac.ke/chemundu','');
+
+---  ADMISSION_RIGHTS
+DROP TABLE IF EXISTS admission_rights CASCADE;
+CREATE TABLE IF NOT EXISTS admission_rights (
+    doctor      bigint NOT NULL CONSTRAINT valid_doctor REFERENCES entities(entity_id),
+    hospital    bigint NOT NULL CONSTRAINT valid_hospital REFERENCES entities(entity_id),
+    note        text
+);
+INSERT INTO admission_rights 
+(doctor,hospital,note)
+VALUES 
+(1,2,'Ask to see Naani'),
+(1,3,'');
+
 
 -- --
 -- AUDIT TABLES
@@ -163,63 +217,7 @@ SELECT username,users.name,users.email,users.telephone,account_number
 FROM users
 WHERE users.active = false;
 
-
---- TYPES
-DROP TABLE IF EXISTS types CASCADE;
-CREATE TABLE IF NOT EXISTS types (
-    type_id      bigserial PRIMARY KEY,
-    title        varchar(100) NOT NULL CONSTRAINT unique_type UNIQUE
-);
-INSERT INTO types (title) VALUES ('Hospital'),('Doctor'),('Dentist');
-
---- SUB_TYPES
-DROP TABLE IF EXISTS sub_types CASCADE;
-CREATE TABLE IF NOT EXISTS sub_types (
-    sub_type_id      bigserial PRIMARY KEY,
-    title            varchar(100) NOT NULL CONSTRAINT unique_sub_type UNIQUE,
-    parent           bigint NOT NULL CONSTRAINT valid_parent_type REFERENCES types(type_id)
-);
-INSERT INTO sub_types(title,parent) VALUES ('Hospital',1),('General Practitioner',2),('Peridontist',3);
-
---- ENITITIES
-DROP TABLE IF EXISTS entities CASCADE;
-CREATE TABLE IF NOT EXISTS entities (
-    entity_id   bigserial PRIMARY KEY,
-    title       varchar(100) NOT NULL,
-    type        bigint NOT NULL CONSTRAINT valid_type REFERENCES sub_types(sub_type_id),
-    location    varchar(255),
-    telephone   varchar(20) CONSTRAINT unique_telephone UNIQUE,
-    office      varchar(20),
-    landline    varchar(20),
-    address     text,
-    fax         varchar(100),
-    email       varchar(50) CONSTRAINT unique_email UNIQUE,
-    web         varchar(100),
-    country     smallint DEFAULT '112',
-    others      text
-);
-INSERT INTO entities 
- (title,type,location,telephone,office,landline,address,fax,email,web,others)
- VALUES
- ('Ian Innocent',3,'Chebarbar','0725678447','072345678','','Text Lane Off Limuru Road','','ianmin2@ianmin2.cf','http://ianmin2.cf',''),
- ('Jeremic Hospital',1,'Baraton, Kapsabet','0700100100','','','1 Jeremic Close University of Eastern Africa Baraton','','jeremic@ueab.ac.ke','http://jeremic.ueab.ac.ke',''),
- ('Chemundu Hospital',1,'Chemundu, Kapsabet','0700100101','','','1 Chemundu Drive; Chemundu','','chemundu@ueab.ac.ke','http://ueab.ac.ke/chemundu','');
-
----  ADMISSION_RIGHTS
-DROP TABLE IF EXISTS admission_rights CASCADE;
-CREATE TABLE IF NOT EXISTS admission_rights (
-    doctor      bigint NOT NULL CONSTRAINT valid_doctor REFERENCES entities(entity_id),
-    hospital    bigint NOT NULL CONSTRAINT valid_hospital REFERENCES entities(entity_id),
-    note        text
-);
-INSERT INTO admission_rights 
-(doctor,hospital,note)
-VALUES 
-(1,2,'Ask to see Naani'),
-(1,3,'');
-
-
--- VIEWS
+-- VW_ENTITIES --
 DROP VIEW IF EXISTS vw_entities CASCADE;
 CREATE OR REPLACE VIEW vw_entities AS 
 SELECT 
@@ -232,7 +230,7 @@ FROM entities
     JOIN types  
         ON sub_types.parent  = types.type_id; 
 
-
+-- VW_ADMISSION-RIGHTS -- 
 DROP VIEW IF EXISTS vw_admission_rights CASCADE;
 CREATE VIEW vw_admission_rights AS 
 SELECT 
