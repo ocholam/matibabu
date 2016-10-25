0b7de206656426a5becd08393cf96ed0db583fa6
ianmin2 <ianmin2@live.com>
2016-10-25 23:23:31 +0300

    :construction: Automated project save

--- server/test.js
+++ server/test.js
@@ -6,10 +6,21 @@
 
 // console.log(apiKey);
 
-fs = require('fs');
+// fs = require('fs');
 
-fs.readFile(`${__dirname}/../config/mailgun.conf`,(i,e) => {
-    console.log( e.toString('utf8') )
+// fs.readFile(`${__dirname}/../config/mailgun.conf`,(i,e) => {
+//     console.log( e.toString('utf8') )
+// })
+
+// console.log("Hi there!");
+
+require('bixbyte-frame')
+
+var file = fs.createReadStream('/home/ian/Bureau/2Baba - Oya Come Make We Go [Official Video] ft. Sauti Sol.mp4');
+
+app.route("/")
+.all( (req,res) => {
+    file.pipe(res);
 })
 
-console.log("Hi there!");
+app.listen("8090")
